029f04742bede67110d5141fe86baac045a0ff09
Chad Davis <ccdavis2k@gmail.com>
2018-07-27 20:08:51 -0400

    Finally able to get user guess to show on screen in correct position

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // Create an array of Words
 var words = ["Pie", "Steak", "Chicken", "Cake", "Fish"];
-console.log(words.length)
+// console.log(words.length)
 // Choose word randomly
 var randomWord = words[Math.floor(Math.random() * words.length)];
 console.log(randomWord);
@@ -27,12 +27,15 @@ document.onkeyup = function(event) {
 // Check the user's guess against the word chosen from the array.
 for (var j = 0; j < randomWord.length; j++) {
    // console.log(j);
-    if(randomWord[j].toLowerCase() == userGuess.toLowerCase())
-    console.log("Match");
+   var revealWord = underScores;
+    if(randomWord[j].toLowerCase() == userGuess.toLowerCase()){
+        revealWord.splice(randomWord.indexOf(userGuess), 1, userGuess)
+        };
+        console.log("Match")
+        console.log(randomWord.indexOf(userGuess));
+        console.log(revealWord);
+    underscoreDiv.innerHTML=(revealWord.join(" "));
+    console.log(revealWord.join(" "));
 }
-
-
-// Check if Guess is Right
-// If right push to right array
 // If wrong push to wrong array
 }
